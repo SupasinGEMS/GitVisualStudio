f5ff940659d5e68527d3ceeb385c3c053fbe6546
SupasinGEMS <gemslua@gmail.com>
2018-05-18 18:34:48 +0700

    reeeee

--- MapWinApp/Form1.vb
+++ MapWinApp/Form1.vb
@@ -42,7 +42,7 @@ Public Class Form1
     End Sub
 
     Private Sub test_Click(sender As Object, e As EventArgs) Handles test.Click
-        Dim dt as New Datable
-        Dim dr as DataRow = nothing
+        Dim dt As New DataTable
+        Dim dr As DataRow = Nothing
     End Sub
 End Class
