325867fe35588c6cbfd106ac90a65ba23b44c757
SupasinGEMS <gemslua@gmail.com>
2018-05-18 15:47:04 +0700

    test0001

--- MapWinApp/Form1.vb
+++ MapWinApp/Form1.vb
@@ -46,6 +46,6 @@ Public Class Form1
         Dim dr As DataRow = Nothing
         Dim CCC As String = "TESTC"
         Dim GEMS As String = "GEMSSSSSSSSSSSSSSSSSSSSSS"
-
+        Dim ZAAA As String = "ZAAAAAAAAAAAAAAAAA"
     End Sub
 End Class
